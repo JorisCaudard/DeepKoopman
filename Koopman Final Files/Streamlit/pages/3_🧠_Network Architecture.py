e572eb8aac56d753d00be3531cc6d03597f604a3
jcaudard17@gmail.com <jcaudard17@gmail.com>
2024-03-23 19:26:28 +0100

    Streamlit commit

--- Koopman Final Files/Streamlit/pages/3_🧠_Network Architecture.py
+++ Koopman Final Files/Streamlit/pages/3_🧠_Network Architecture.py
@@ -2,21 +2,28 @@ import streamlit as st
 
 st.title("Network architecture")
 
-tab1, tab2, tab3 = st.tabs(["Auto-Encoder", "Architecture Simple", "Architecture Générale"])
+tab1, tab2 = st.tabs(["Structure théorique", "Aperçu du code"])
 
 with tab1:
-    st.write("L'architecture générale du réseau proposé pour l'identification de l'opérateur de Koopman repose sur l'érchitecture de l'auto-encoder simple.")
-
-    #st.image() ##TODO: Import Auto-encoder picture
-
-    st.write("A cette structure simple, on rajoute un avancement linéaire dans le temps permettant l'identification de l'opérateur de Koopman")
+    st.header("Architecture de l'AutoEncoder DeepKoopman", divider= True)
+    st.image("https://media.springernature.com/full/springer-static/image/art%3A10.1038%2Fs41467-018-07210-0/MediaObjects/41467_2018_7210_Fig1_HTML.png?as=webp")
+    
+    st.markdown(r'''L'architecture proposée dans le cadre de l'apprentissage de l'opérateur de Koopman se base sur une structure classique d'Auto-encoder. 
+                L'objectif étant d'identifier un opérateur linéaire dans un espace d'état latent, l'espace latent de l'auto-encoder prend la forme d'une matrice, qu'on assimile dans l'implémentation python à une couche Linéaire sans biais.''')
+    
+    st.markdown(r'''Le réseau est basé sur un autoencodeur qui est capable d'identifier les coordonnées intrinsèques 
+    $y = \varphi (x)$ et de décoder ces coordonnées pour récupérer $x = \varphi^{-1}(y)$.''')
+    
+    st.image("https://media.springernature.com/full/springer-static/image/art%3A10.1038%2Fs41467-018-07210-0/MediaObjects/41467_2018_7210_Fig2_HTML.png?as=webp")
+    
+    st.markdown(r'''L'ajout d'un réseau auxiliaire permet d'identifier le spectre des valeurs propres continues de l'opérateur $K$. Cela facilite 
+    une réduction de dimension agressive dans l'auto-encodeur, évitant ainsi le besoin de plus hautes harmoniques de la fréquence 
+    qui sont générées par la non-linéarité.''')
 
 with tab2:
-    st.header("Structure Basique")
+    st.header("Implémentation en Python", divider= True)
 
-    #st.image() ##TODO: Import Simple Koopman Network picture
-
-    st.write("L'implémentation de ce réseau simple de Koopman a été faite en utilisant les focntionnalités du module python Pytorch.")
+    st.markdown(r"L'implémentation de ce réseau simple de Koopman a été faite en utilisant les focntionnalités du module python Pytorch.")
 
     st.code("""class SimpleKoopmanNeuralNetwork(nn.Module):
     def __init__(self, params):
@@ -76,7 +83,4 @@ with tab2:
         #We get both the encoded trajectory and the decoded trajectory
         return trajectoryPrediction, latentTrajectoryPrediction""")
     
-    st.write("Ce réseau prend en input une condition initiale du système dynamique étudié, et permet de prédire en sortie deux objets distincts : D'une part, la trajectoire à partir de cet état initial dans l'espace latent, d'autre part la trajectoire prédite dans l'espace initial.")
-
-#with tab3:
-    #TODO: Add description of the general architecture
+    st.markdown(r"Ce réseau prend en entrée une condition initiale du système dynamique étudié, et permet de prédire en sortie la trajectoire du système dynamique sur un nombre de pas défini grâce au dictionnaire d'initialisation $params$.")
